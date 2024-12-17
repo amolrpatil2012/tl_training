ef6def7c950d66fe43cc102d494894857d986b93
AMOL PATIL <136699553+amolrpatil2012@users.noreply.github.com>
2024-12-17 11:12:53 +0530

    Update InheritanceDemo.java

--- OOPConcepts/src/InheritanceDemo.java
+++ OOPConcepts/src/InheritanceDemo.java
@@ -1,4 +1,10 @@
 
 public class InheritanceDemo {
-
+public static void main(String[] args) {
+		
+		BoxColor b = new BoxColor(10,20,10,"Red");
+		System.out.println(" Area of Box : " + b.area());
+		System.out.println( " Volume of Box : " + b.volume());
+		System.out.println(" Color of Box : " + b.getColor());
+	}
 }
